9db485e30a8f19e84a758a7920040d7f0d502f7d
chiefkarim <krimotop2016@gmail.com>
2024-07-07 15:07:15 +0100

    chore: remove dist

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import Showdown from "showdown";
 
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("lol");
   fetch("content.md")
     .then((response) => response.text())
     .then((text) => {
-      console.log("text", text);
       var converter = new Showdown.Converter();
       var html = converter.makeHtml(text);
       document.getElementById("content").innerHTML = html;
